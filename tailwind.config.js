de65749858686930bd93d2b55565189b3e8eb399
Greg Mozer <mozer.grzegorz@yahoo.com>
2022-02-13 14:27:46 -0500

    finish coming soon page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#A32035",
+        navy: "#005c82",
+        black: "#2f4858",
+      },
+    },
   },
   plugins: [],
 };
