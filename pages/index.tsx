75a0991b64f55785157ac75977d78592fb8b76c2
Greg Mozer <mozer.grzegorz@yahoo.com>
2022-02-14 00:26:14 -0500

    update classes for Hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,49 @@ import { Layout } from "../components/layout/Layout";
 import backgroundImage from "../public/img/coming-soon/josiah-farrow.avif";
 import logo from "../public/img/logo.svg";
 
+const styles = {
+  wrapper: "flex w-screen h-screen gradient z-[-1]",
+  image: "fixed w-full h-full overflow-hidden hidden md:block",
+  container: "container mx-auto p-6 md:px-0 mt-24 md:mt-28 z-auto",
+  card: "opacity-50 hover:opacity-100 flex flex-col items-center p-10 bg-white rounded-md shadow-lg max-w-lg md:max-w-4xl md:p-20 transition delay-150 ease-in duration-700",
+};
+
+const Hero = () => {
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.image}>
+        <Image
+          src={backgroundImage}
+          blurDataURL=""
+          alt="Photo by Josiah Farrow on Unsplash"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          placeholder="blur"
+        />
+      </div>
+      <div className={styles.container}>
+        <div id="card" className={styles.card}>
+          <div className="flex-1 mt-5 text-center md:mt-10">
+            <h3 className="text-xl font-bold uppercase md:text-5xl text-navy">
+              Coming Soon!
+            </h3>
+            <p className="mt-5 text-xl md:mt-10 md:text-2xl">
+              <span className="font-bold">Farah</span> Freight Group is a
+              trucking company providing transportation services in Canada and
+              U.S.A.
+            </p>
+            <p className="mt-5 text-xl md:mt-10 md:text-3xl">Contact us:</p>
+            <p className="mt-2 text-2xl font-bold md:mt-4 md:text-5xl">
+              416-702-3267
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 const Home: NextPage = () => {
   const seo = {
     description:
@@ -18,54 +61,7 @@ const Home: NextPage = () => {
         <title>Farah Freight Group</title>
       </Head>
       <Layout seo={seo}>
-        <div className="flex items-center justify-center w-screen h-screen gradient z-[-1]">
-          <div className="fixed w-full h-full overflow-hidden opacity-75">
-            <Image
-              src={backgroundImage}
-              blurDataURL=""
-              alt="Photo by Josiah Farrow on Unsplash"
-              layout="fill"
-              objectFit="cover"
-              quality={100}
-              placeholder="blur"
-            />
-          </div>
-          <div
-            id="card"
-            className="opacity-75 hover:opacity-100 absolute flex flex-col items-center justify-center w-screen p-10 bg-white rounded-md shadow-lg md:max-w-4xl md:p-20"
-          >
-            <div id="logo" className="relative flex gap-2 mr-3 uppercase">
-              <div className="w-[72px] h-[36px] md:w-[144px] md:h-[72px] relative">
-                <Image
-                  src={logo}
-                  alt="Farah Freight Group"
-                  quality={100}
-                  objectFit="cover"
-                  layout="fill"
-                />
-              </div>
-              <div className="self-end text-xl md:text-4xl">
-                <h2>
-                  <span className="font-bold">Farah</span> Freight Group
-                </h2>
-              </div>
-            </div>
-            <div className="flex-1 mt-5 text-center md:mt-10">
-              <h3 className="text-xl font-bold uppercase md:text-5xl text-navy">
-                Coming Soon!
-              </h3>
-              <p className="mt-5 text-xl md:mt-10 md:text-2xl">
-                <span className="font-bold">Farah</span> Freight Group is a
-                trucking company providing transportation services in Canada and
-                U.S.A.
-              </p>
-              <p className="mt-5 text-xl md:mt-10 md:text-3xl">Contact us:</p>
-              <p className="mt-2 text-2xl font-bold md:mt-4 md:text-5xl">
-                416-702-3267
-              </p>
-            </div>
-          </div>
-        </div>
+        <Hero />
       </Layout>
     </>
   );
