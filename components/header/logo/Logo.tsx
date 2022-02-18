aec14645cac851e25f0eed1ea81830668e5848db
Greg Mozer <mozer.grzegorz@yahoo.com>
2022-02-18 00:44:14 -0500

    add link to home page when logo clicked

--- components/header/logo/Logo.tsx
+++ components/header/logo/Logo.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
 import logo from "@/public/img/logo.svg";
+import Link from "next/link";
 
 export const Logo: React.FC = () => {
   return (
     <div id="logo" className="relative flex gap-2 mr-3 uppercase items-center">
-      <div className="w-[72px] h-[36px] md:w-[104px] md:h-[52px] relative">
-        <Image
-          src={logo}
-          alt="Farah Freight Group"
-          quality={100}
-          objectFit="cover"
-          layout="fill"
-        />
-      </div>
+      <Link href="/">
+        <a className="w-[72px] h-[36px] md:w-[104px] md:h-[52px] relative">
+          <Image
+            src={logo}
+            alt="Farah Freight Group"
+            quality={100}
+            objectFit="cover"
+            layout="fill"
+          />
+        </a>
+      </Link>
+
       <div className="self-end md:text-xl max-w-[175px]">
         <h2>
           <span className="font-bold">Farah</span> Freight Group
